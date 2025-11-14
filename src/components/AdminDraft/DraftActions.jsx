28504c6e42de3c6732aec61c544ca69e5b53fff7
Juan Miguel Ramirez <ramirezjuanmiguel032@gmail.com>
2025-11-14 22:13:46 +0800

    some adjustment in size of the box draft-articles

--- src/components/AdminDraft/DraftActions.jsx
+++ src/components/AdminDraft/DraftActions.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Stack, Group, ActionIcon, Text, Modal, Button, Title } from "@mantine/core";
+import { Stack, Group, ActionIcon, Text, Modal, Button, Title, Box } from "@mantine/core";
 import { notifications } from "@mantine/notifications";
 import { IconEdit, IconTrash, IconUpload } from "@tabler/icons-react";
 
@@ -7,10 +7,9 @@ export default function DraftActions({ onEdit, onDelete, onPublish }) {
   const [deleteModalOpened, setDeleteModalOpened] = useState(false);
 
   const handleConfirmDelete = () => {
-    onDelete?.(); // call parent delete function
+    onDelete?.();
     setDeleteModalOpened(false);
 
-    // Show notification
     notifications.show({
       title: "Draft deleted",
       message: "Draft deleted successfully!",
@@ -21,7 +20,6 @@ export default function DraftActions({ onEdit, onDelete, onPublish }) {
   const handlePublish = () => {
     onPublish?.();
 
-    // Show notification
     notifications.show({
       title: "Draft published",
       message: "Draft published successfully!",
@@ -31,42 +29,53 @@ export default function DraftActions({ onEdit, onDelete, onPublish }) {
 
   return (
     <>
-      <Stack spacing="sm" align="flex-start" style={{ marginTop: "10px" }}>
-        {/* Edit */}
-        <Group spacing={4} align="center">
-          <ActionIcon variant="subtle" color="gray" onClick={onEdit}>
-            <IconEdit size={18} />
-          </ActionIcon>
-          <Text size="sm" style={{ cursor: "pointer" }} onClick={onEdit}>
-            Edit
-          </Text>
-        </Group>
+      {/* Box wrapper to control size */}
+      <Box
+        style={{
+          width: "120px",      
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "10px",
+        }}
+      >
+        {/* Actions stack */}
+        <Stack spacing="sm" align="flex-start" style={{ width: "100%" }}>
+          {/* Edit */}
+          <Group spacing={4} align="center">
+            <ActionIcon variant="subtle" color="gray" onClick={onEdit}>
+              <IconEdit size={18} />
+            </ActionIcon>
+            <Text size="sm" style={{ cursor: "pointer" }} onClick={onEdit}>
+              Edit
+            </Text>
+          </Group>
 
-        {/* Delete */}
-        <Group spacing={4} align="center">
-          <ActionIcon variant="subtle" color="red" onClick={() => setDeleteModalOpened(true)}>
-            <IconTrash size={18} />
-          </ActionIcon>
-          <Text
-            size="sm"
-            color="red"
-            style={{ cursor: "pointer" }}
-            onClick={() => setDeleteModalOpened(true)}
-          >
-            Delete
-          </Text>
-        </Group>
+          {/* Delete */}
+          <Group spacing={4} align="center">
+            <ActionIcon variant="subtle" color="red" onClick={() => setDeleteModalOpened(true)}>
+              <IconTrash size={18} />
+            </ActionIcon>
+            <Text
+              size="sm"
+              color="red"
+              style={{ cursor: "pointer" }}
+              onClick={() => setDeleteModalOpened(true)}
+            >
+              Delete
+            </Text>
+          </Group>
 
-        {/* Publish */}
-        <Group spacing={4} align="center">
-          <ActionIcon variant="subtle" color="blue" onClick={handlePublish}>
-            <IconUpload size={18} />
-          </ActionIcon>
-          <Text size="sm" color="blue" style={{ cursor: "pointer" }} onClick={handlePublish}>
-            Publish
-          </Text>
-        </Group>
-      </Stack>
+          {/* Publish */}
+          <Group spacing={4} align="center">
+            <ActionIcon variant="subtle" color="blue" onClick={handlePublish}>
+              <IconUpload size={18} />
+            </ActionIcon>
+            <Text size="sm" color="blue" style={{ cursor: "pointer" }} onClick={handlePublish}>
+              Publish
+            </Text>
+          </Group>
+        </Stack>
+      </Box>
 
       {/* Delete confirmation modal */}
       <Modal
@@ -74,6 +83,7 @@ export default function DraftActions({ onEdit, onDelete, onPublish }) {
         onClose={() => setDeleteModalOpened(false)}
         centered
         withCloseButton={false}
+        size={360}
         styles={{
           header: { display: "none" },
           body: {
